0613202edcf8b91537138a28175895dc12ee895a
megis7 <megisoglou56@gmail.com>
2019-07-24 20:37:09 +0300

    Parser experiments with minibasic.

--- compiler/Program.fs
+++ compiler/Program.fs
@@ -5,21 +5,35 @@ open Tokens
 
 [<EntryPoint>]
 let main argv =
-    let readLexemes str =
-      let lexbuf = LexBuffer<_>.FromString str
-      let rec aux lexbuf =
-        let x = Lexer.read lexbuf
-        if x = EOF then []
-        else x :: aux lexbuf
+    // let readLexemes str =
+    //   let lexbuf = LexBuffer<_>.FromString str
+    //   let rec aux lexbuf =
+    //     let x = Lexer.read lexbuf
+    //     if x = EOF then []
+    //     else x :: aux lexbuf
+    //   aux lexbuf
+     
+    let parse input =
+      let lexbuf = LexBuffer<_>.FromString input
+      let res = Parser.start Lexer.read lexbuf
+      res
 
-      aux lexbuf
-
-    let input = System.IO.File.ReadAllText "../examples/lexerror.mba"
+    let input = System.IO.File.ReadAllText "../examples/hello.mba"
 
     try
-      let lexemes = readLexemes input
-      for lexeme in lexemes do
-        printfn "%s" <| MinibasicToken.ToString lexeme
+      let l = parse input
+      match l with
+      | Some stmts ->
+        for stmt in stmts do
+          printfn "%A" stmt
+      | None -> printfn "No input"
+      
     with
       | e -> printfn "Error occured: \n%s" (e.Message)
+    // try
+    //   let lexemes = readLexemes input
+    //   for lexeme in lexemes do
+    //     printfn "%s" <| MinibasicToken.ToString lexeme
+    // with
+    //   | e -> printfn "Error occured: \n%s" (e.Message)
     0
--- compiler/Tokens.fs
+++ compiler/Tokens.fs
@@ -3,17 +3,28 @@ module Tokens
 // type ConstType = 
 //     | Integer of int
 
-type MinibasicToken =
-    | Command of string
-    | Identifier of string
-    | Constant of int
-    | Operator of char
-    | EOF
+type MinibasicExpression =
+    | Expr of string
+    | Int of int
 
-    static member ToString x =
+// type MinibasicToken =
+//     | Command of string
+//     | Identifier of string
+//     | Constant of int
+//     | Operator of char
+//     | EOF2
+
+//     static member ToString x =
+//         match x with
+//         | Command c -> sprintf "Command(%s)" c
+//         | Identifier i -> sprintf "Identifier(%s)" i
+//         | Constant c -> sprintf "Const(%i)" c
+//         | Operator o -> sprintf "Operator(%c)" o
+//         | _ -> ""
+
+type MinibasicStatement =
+    | Print of MinibasicExpression
+
+    static member ToString x = 
         match x with
-        | Command c -> sprintf "Command(%s)" c
-        | Identifier i -> sprintf "Identifier(%s)" i
-        | Constant c -> sprintf "Const(%i)" c
-        | Operator o -> sprintf "Operator(%c)" o
-        | _ -> ""
+        | Print s -> sprintf "Print(%A)" s

888fd62cdc3044a7dbfc4d7c92fc7f720f4b3ff0
megis7 <megisoglou56@gmail.com>
2019-07-25 12:23:27 +0300

    First attempt for lexer of PCL.

--- compiler/Program.fs
+++ compiler/Program.fs
@@ -1,46 +1,47 @@
 ﻿module Program
 
 open FSharp.Text.Lexing
-open Tokens
+open Parser
 
 open LLVMSharp
 
 [<EntryPoint>]
 let main argv =
-    // let readLexemes str =
-    //   let lexbuf = LexBuffer<_>.FromString str
-    //   let rec aux lexbuf =
-    //     let x = Lexer.read lexbuf
-    //     if x = EOF then []
-    //     else x :: aux lexbuf
-    //   aux lexbuf
+    let readLexemes str =
+      let lexbuf = LexBuffer<_>.FromString str
+      let rec aux lexbuf =
+        let x = Lexer.read lexbuf
+        if x = EOF then []
+        else x :: aux lexbuf
+      aux lexbuf
      
-    let parse input =
-      let lexbuf = LexBuffer<_>.FromString input
-      let res = Parser.start Lexer.read lexbuf
-      res
+    // let parse input =
+    //   let lexbuf = LexBuffer<_>.FromString input
+    //   let res = Parser.start Lexer.read lexbuf
+    //   res
 
-    let input = System.IO.File.ReadAllText "../examples/hello.mba"
-
-    try
-      let l = parse input
-      match l with
-      | Some stmts ->
-        for stmt in stmts do
-          printfn "%A" stmt
-      | None -> printfn "No input"
-      
-    with
-      | e -> printfn "Error occured: \n%s" (e.Message)
-
-    let modu = LLVM.ModuleCreateWithName "LLVMSharpIntro"
-
-    LLVM.PrintModuleToFile (modu, "test.txt", ref null) |> ignore
+    // let input = System.IO.File.ReadAllText "../examples/hello.mba"
 
     // try
-    //   let lexemes = readLexemes input
-    //   for lexeme in lexemes do
-    //     printfn "%s" <| MinibasicToken.ToString lexeme
+    //   let l = parse input
+    //   match l with
+    //   | Some stmts ->
+    //     for stmt in stmts do
+    //       printfn "%A" stmt
+    //   | None -> printfn "No input"
+      
     // with
     //   | e -> printfn "Error occured: \n%s" (e.Message)
+
+    let input = System.IO.File.ReadAllText "../examples/hello.mba"
+    let lexemes = readLexemes input
+
+    for lexeme in lexemes do
+      printfn "%A" lexeme
+
+    (* LLVM *)
+    // let modu = LLVM.ModuleCreateWithName "LLVMSharpIntro"
+
+    // LLVM.PrintModuleToFile (modu, "test.txt", ref null) |> ignore
+
     0
--- compiler/Tokens.fs
+++ compiler/Tokens.fs
@@ -7,21 +7,6 @@ type MinibasicExpression =
     | Expr of string
     | Int of int
 
-// type MinibasicToken =
-//     | Command of string
-//     | Identifier of string
-//     | Constant of int
-//     | Operator of char
-//     | EOF2
-
-//     static member ToString x =
-//         match x with
-//         | Command c -> sprintf "Command(%s)" c
-//         | Identifier i -> sprintf "Identifier(%s)" i
-//         | Constant c -> sprintf "Const(%i)" c
-//         | Operator o -> sprintf "Operator(%c)" o
-//         | _ -> ""
-
 type MinibasicStatement =
     | Print of MinibasicExpression
 

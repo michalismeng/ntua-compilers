853d9bf6c9f648de8adb6654c2342c62bff9456f
megis7 <megisoglou56@gmail.com>
2019-11-11 20:05:39 +0200

    Restructured testing folder.

--- testing/test.py
+++ testing/test.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+# Script that runs the test suite for our compiler implementation.
+# PCL source files are located in the programs folder while standard input and output files are located in the corresponding folders.
+# Eg. the file programs/program.pcl will be compiled and then run two times, once with input inputs/program.1.in and the second with inputs/program.2.in
+# the outputs will be checked against outputs/program.{1,2}.out respectively.
+
+import os.path
 import subprocess
 import glob
 
@@ -9,8 +15,17 @@ llcOutputPath = "test.asm"
 libPath = "lib.a"
 clangOutputPath = "a.out"
 
-for fprog in glob.glob("*.pcl"):
-    progName = fprog[0:fprog.find(".")]
+if os.path.exists(compilerPath) == False:
+    print ('The compiler under test does not exist. Please add a ./compiler executable in the current directory')
+    exit (1)
+
+if os.path.exists(libPath) == False:
+    print ('The compiler library does not exist. Please add a ./lib.a archive in the current directory')
+    exit (1)
+
+# iterate over all PCL source files
+for fprog in glob.glob("programs/*.pcl"):
+    progName = fprog[fprog.find('/') + 1:fprog.find(".")]
 
     print("testing %s" % progName)
 
@@ -21,20 +36,23 @@ for fprog in glob.glob("*.pcl"):
     compileOutput = compileProc.stdout.read()
 
     if compileResult != 0:
-        print ("Error on compilation")
+        print ("Error on compilation of %s" % progName)
         exit (1)
         
-    with open(compilerOutputPath, 'r') as f:
-        llvmProrgam = f.read()
+    # LLVM output of the compiler for the current PCL source file
+    # with open(compilerOutputPath, 'r') as f:
+    #     llvmProrgam = f.read()
 
     subprocess.call("llc %s -o %s" % (compilerOutputPath, llcOutputPath), shell=True)
     subprocess.call("clang %s %s -o %s" % (llcOutputPath, libPath, clangOutputPath), shell=True)
 
     errors = 0
-    for fin in glob.glob("%s.*.in" % progName):
-        fout = "%s.out" % fin[0:fin.rfind('.')]
+    # iterate over all input files for the currently compiled PCL program
+    for fin in glob.glob("inputs/%s.*.in" % progName):
+        fout = "outputs/%s.out" % fin[fin.find('/') + 1:fin.rfind('.')]
         with open(fin) as ffin:
             output = subprocess.Popen("./a.out", stdin=ffin, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+            # check against the desired output, which is found in the outputs folder
             with open(fout) as ffout:
                 expected = ffout.read()
                 if expected != output:
@@ -47,4 +65,5 @@ for fprog in glob.glob("*.pcl"):
     else:
         print("Found %d errors" % errors)
 
+# remove test.txt, test.asm and a.out which were created while compiling the test programs
 subprocess.call("rm %s %s %s" % (compilerOutputPath, llcOutputPath, clangOutputPath), shell=True)
